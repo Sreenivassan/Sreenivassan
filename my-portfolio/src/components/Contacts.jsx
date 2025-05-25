5f3a5a861e608be14145ebf91d1092aad1200b70
unknown <aerosreeni97@gmail.com>
2025-05-25 23:10:56 +0530

    May-Build

--- my-portfolio/src/components/Contacts.jsx
+++ my-portfolio/src/components/Contacts.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Contacts() {
   return (
-   <section className="py-16 px-8 bg-white" id="contact">
-    <h2 className="text-3xl font-semibold mb-4">Contact</h2>
-    <p>Phone: <a href="tel:+917373899569" className="text-blue-500">+91 73738 99569</a></p>
-    <p>Email: <a href="mailto:aerosreeni97@gmail.com" className="text-blue-500">aerosreeni97@gmail.com</a></p>
-    <p>Location: Coimbatore, India</p>
-  </section>
-  )
+    <section className="py-16 px-8 bg-white" id="contact">
+      <h2 className="text-3xl font-semibold mb-4">Contact</h2>
+      <p>
+        Phone:{" "}
+        <a href="tel:+917373899569" className="text-blue-500">
+          +91 73738 99569
+        </a>
+      </p>
+      <p>
+        Email:{" "}
+        <a href="mailto:aerosreeni97@gmail.com" className="text-blue-500">
+          aerosreeni97@gmail.com
+        </a>
+      </p>
+      <p>Location: Coimbatore, India</p>
+    </section>
+  );
 }
 
-export default Contacts
+export default Contacts;
