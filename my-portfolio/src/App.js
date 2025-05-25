d48fb98d09ff36d669dcd9bbf2c3903bf588e559
unknown <aerosreeni97@gmail.com>
2025-05-25 21:44:44 +0530

    Changes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,22 +1,22 @@
-import React, { Fragment } from 'react'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contacts from './components/Contacts'
-import Skills from './components/Skills'
-import Navbar from './components/Navigation'
+import React, { Fragment } from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
+import Skills from "./components/Skills";
+import Navbar from "./components/Navigation";
 
 function App() {
   return (
-   <Fragment>
-    <Navbar/>
-    <Hero/>
-    <About/>
-    <Projects/>
-    <Skills/>
-    <Contacts/>
-   </Fragment>
-  )
+    <Fragment>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contacts />
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
